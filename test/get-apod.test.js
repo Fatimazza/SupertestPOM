48db9cdb94b9ccc2bc4006903a4e36d23f501991
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-05-27 15:34:07 +0700

    Apod Picture - Add positive and negative Test Case

---
+++ test/get-apod.test.js
@@ -0,0 +1,9 @@
+const testCases = {
+    describe: 'As a User, I want to make sure Value is correct',
+    positive: {
+        validParams: 'Verify True equal True'
+    },
+    negative: {
+        validParams: 'Verify False equal False'
+    }
+}
