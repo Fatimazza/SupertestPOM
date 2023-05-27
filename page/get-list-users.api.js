7fe72f14d945f87912de8f81b6c1d36bc6e1409b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-05-27 16:47:32 +0700

    List Users - Fix Env and API GET request List Users

--- page/get-list-users.api.js
+++ page/get-list-users.api.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-const env = require('dotenv').config;
-const api = request(process.env.BASE_URL);
+const env = require('dotenv').config();
+const api = request(process.env.BASE_URL_REQRES);
 
 const getListUsers = (payload) =>
     api
