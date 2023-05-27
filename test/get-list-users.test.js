b411f4f31d721664ad3860314a743f704fe597bd
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-05-27 16:34:43 +0700

    List Users - Add Assertion to Test Case

--- test/get-list-users.test.js
+++ test/get-list-users.test.js
@@ -1,6 +1,14 @@
+const assert = require('chai').expect;
+
 const testCases = {
     describe: 'As a User, I want to see the list of users',
     positive: {
         validParams: 'Verify the data of users displayed'
     }
 };
+
+describe(`@get ${testCases.describe}`, () => {
+    it(`@positive ${testCases.positive.validParams}`, async () => {
+        assert(true).to.equal(true);
+    });
+});
