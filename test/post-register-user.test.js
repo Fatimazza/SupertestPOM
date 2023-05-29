2af5a523949427d45f9faa89b4c6a390677408c3
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-05-29 07:49:30 +0700

    Register User - Fix API POST request Register User using payload

--- test/post-register-user.test.js
+++ test/post-register-user.test.js
@@ -1,5 +1,12 @@
 const assert = require('chai').expect;
 
+const page = require('../page/post-register-user.api.js');
+
+let payload = {
+    'email': 'eve.holt@reqres.in',
+    'password': 'pistol'
+};
+
 const testCases = {
     describe: 'As a User, I want to register to Reqres.in',
     positive: {
@@ -9,6 +16,7 @@ const testCases = {
 
 describe(`@get ${testCases.describe}`, () => {
     it(`@positive ${testCases.positive.validParams}`, async () => {
-        assert(true).to.equal(true);
+        const response = await page.postRegisterUser(payload);
+        assert(response.status).to.equal(200);
     });
 });
