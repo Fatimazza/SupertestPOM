4a22621b9f6870d2e6a28014c1b06b727db07722
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-05-29 07:45:40 +0700

    Register User - Add Assertion to Test Case

--- test/post-register-user.test.js
+++ test/post-register-user.test.js
@@ -1,6 +1,14 @@
+const assert = require('chai').expect;
+
 const testCases = {
     describe: 'As a User, I want to register to Reqres.in',
     positive: {
         validParams: 'Verify new user successfully registered'
     }
 };
+
+describe(`@get ${testCases.describe}`, () => {
+    it(`@positive ${testCases.positive.validParams}`, async () => {
+        assert(true).to.equal(true);
+    });
+});
