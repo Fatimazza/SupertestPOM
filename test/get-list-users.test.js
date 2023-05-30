fafaa4bd305325ea3147a966a2c4a90d312a30dc
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-05-30 17:12:30 +0700

    List Users - Add JSON Schema Assertion

--- test/get-list-users.test.js
+++ test/get-list-users.test.js
@@ -1,7 +1,10 @@
 const { expect } = require('chai');
 const assert = require("chai").expect;
+const chai = require('chai');
+chai.use(require('chai-json-schema'));
 
 const page = require('../page/get-list-users.api');
+const schema = require('../schema/get-list-users.schema.js')
 
 const testCases = {
     describe: 'As a User, I want to see the list of users',
@@ -29,5 +32,7 @@ describe(`@get ${testCases.describe}`, () => {
             expect(element)
                 .to.have.all.keys(['id', 'email', 'avatar', 'first_name', 'last_name']);
         });
+
+        expect(response.body).to.be.jsonSchema(schema.LIST_USERS_SCHEMA);
     });
 });
